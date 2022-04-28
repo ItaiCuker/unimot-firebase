a13efd1026315651dd9d109e15ddafc639bbea81
itai <itano1234@gmail.com>
2022-04-28 19:05:32 +0300

    added cloud function that lists all devices

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,57 +2,172 @@
 /* eslint-disable max-len */
 import * as functions from "firebase-functions";
 
+// firestore db import
+import admin = require("firebase-admin");
+admin.initializeApp();
+const FIREBASE_CONFIG = process.env.FIREBASE_CONFIG && JSON.parse(process.env.FIREBASE_CONFIG);
+const projectId = FIREBASE_CONFIG.projectId;
+const db = admin.firestore();
+const remotesRef = db.collection("remotes");
+const devicesRef = db.collection("devices");
+const commandsRef = db.collection("commands");
+
+// IoT Core client import
 import iot = require("@google-cloud/iot");
 const client = new iot.v1.DeviceManagerClient();
 
+/**
+ * cloud function to send command to a remote
+ */
 export const sendCommand = functions.region("europe-west1").runWith({
   minInstances: 1,
   memory: "128MB",
-}).https.onCall((data) => {
-  const deviceId = data.deviceId; // id that client sent
-  const command = data.command; // command that client sent
-  console.log("deviceId: %s \t command %s", deviceId, command);
-
-  // checking for invalid arguments:
-
-  if (!(typeof command === "string") || command.length === 0 || !(typeof deviceId === "string") || deviceId.length === 0) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new functions.https.HttpsError("invalid-argument", "The function recieved invalid arguments");
-  }
-
-  // sending command if no errors
-  return sendToDevice(deviceId, command);
+}).https.onCall((req) => {
+  console.log("sendCommand: ", req);
+  return sendToRemote(req);
 });
 
 /**
- *
- * @param deviceId device id to send command to
- * @param command command to send to device
- * @returns reponse of function
+ * function to send command to remote
+ * @param req request to send to remote
+ * @returns promise of sendCommandToDevice function
  */
-async function sendToDevice(deviceId:string, command:string) {
+async function sendToRemote(req: any) {
   const formattedName = client.devicePath(
-      await client.getProjectId(), // project id
+      projectId, // project id
       "europe-west1", // client region
       "remotes", // registry id, my project uses only one.
-      deviceId // device id
+      req.remoteId // remote id
   );
 
-  const binaryData = Buffer.from(command); // parsing command string data to binary buffer
+  if (req.message === "send") {
+    const command = (await commandsRef.doc(req.commandId).get()).data();
+
+    console.log("remote command =", command);
+    req.command = command;
+  }
+
+
+  const binaryData = Buffer.from(JSON.stringify(req.command)); // parsing JSON to binary buffer.
 
   const request = {
     name: formattedName,
     binaryData: binaryData,
   };
   console.log("sending command:");
-  const [response] = await client.sendCommandToDevice(request);
-  console.log("Sent command: ", (response) ? "succesful" : response);
-  return response;
+  return new Promise((resolve, reject) => {
+    client.sendCommandToDevice( request, (err: unknown, resp: unknown) => {
+      if (err) {
+        console.log("Could not send command:", req);
+        console.log("Error: ", err);
+        reject(err);
+      } else {
+        resolve(resp);
+      }
+    });
+  });
 }
 
-exports.devicePublish = functions.region("europe-west1").runWith({
+/**
+ * cloud function to run when an IoT Core device publishes data
+ */
+exports.onRemotePublish = functions.region("europe-west1").runWith({
   minInstances: 1,
   memory: "128MB",
-}).pubsub.topic("remote-data").onPublish((message) => {
-  console.log("Publish message: ", message.attributes, Buffer.from(message.data, "base64"));
+}).pubsub.topic("remote-data").onPublish(async (message) => {
+  const promises: Promise<FirebaseFirestore.WriteResult>[] = []; // array of promises
+
+  const remoteId = message.attributes.deviceId; // remoteId string
+
+  const decodedData = Buffer.from(message.data, "base64").toString();
+  const dataJson = JSON.parse(decodedData); // parsing data base64 encoded to JSON.
+  console.log("Publish message: {%s,%s}", remoteId, dataJson);
+
+  // create command
+  if (dataJson.state === "create") {
+    const commandId: string = dataJson.commandId;
+    const commandName: string = dataJson.commandName;
+
+    // creating command:
+    const commandRef = !commandId ? commandsRef.doc() : commandsRef.doc(commandId); // creating command doc if commandId is empty string
+    promises.push(commandRef.set(
+        {
+          commandName: commandName,
+          commandLen: dataJson.commandLen,
+          command: dataJson.command,
+        }
+    ));
+
+    // updating device and remote:
+    promises.push(devicesRef.doc(dataJson.deviceId).update(`commands.${commandName}`, commandRef.id));
+    promises.push(remotesRef.doc(remoteId).update({state: ""}));
+  } else {
+    // any other publish message will update remote doc
+    promises.push(remotesRef.doc(remoteId).update(dataJson));
+
+    // updating all devices linked to remote
+    (await updateDevicesStates(dataJson.state === "", remoteId)).forEach((promise) => {
+      promises.push(promise);
+    });
+  }
+  return Promise.all(promises);
 });
+
+/**
+ * cloud function to run periodically to update remotes and devices state.
+ */
+exports.getRemoteState = functions.region("europe-west1").pubsub.schedule("every 1 minutes").onRun( async () => {
+  const promises: Promise<FirebaseFirestore.WriteResult>[] = []; // array of promises
+  (await remotesRef.get()).forEach(async (doc) => {
+    // getting state remote for every remote in collection
+    const remoteId = doc.id;
+
+    let isOnline: boolean;
+    const request = {
+      remoteId: remoteId,
+      command: {alive: "?"},
+    };
+    try {
+      await sendToRemote(request);
+      console.log("Assuming device is online after succecfull alive? command");
+      isOnline = true;
+    } catch (error) {
+      console.log("Unable to send alive? command", error);
+      isOnline = false;
+    }
+
+    // remote doc reference
+    const remoteDoc = remotesRef.doc(remoteId);
+
+    // updating isOnline field
+    promises.push(remoteDoc.update({isOnline: isOnline}));
+
+    // getting state field from remoteId doc
+    const state: string = (await remoteDoc.get()).get("state");
+    // assigning isAvailable based these, while checking if state is null:
+    const isAvailable = !state && isOnline;
+
+    // updating all devices linked to remote
+    (await updateDevicesStates(isAvailable, remoteId)).forEach((promise) => {
+      promises.push(promise);
+    });
+  });
+  return Promise.all(promises);
+});
+
+
+/**
+ * updates all devices linked to remote
+ * @param isAvailable state boolean
+ * @param remoteId id of remote
+ */
+async function updateDevicesStates(isAvailable: boolean, remoteId: string) {
+  const promises: Promise<FirebaseFirestore.WriteResult>[] = []; // array of promises
+  // updating all Device documents state that are linked to the Remote
+  (await devicesRef.where("remoteId", "==", remoteId).get())
+      .forEach((deviceSnap) => {
+        // updating isAvailable field for all Devices linked to Remote
+        promises.push(devicesRef.doc(deviceSnap.id).update({isAvailable: isAvailable}));
+      });
+  return promises;
+}
