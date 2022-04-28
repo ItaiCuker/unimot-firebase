91717777968146cfa570c47267a141bc61fad981
itai <itano1234@gmail.com>
2022-04-28 19:05:32 +0300

    added cloud function that lists all devices

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,45 @@ import * as functions from "firebase-functions";
 import iot = require("@google-cloud/iot");
 const client = new iot.v1.DeviceManagerClient();
 
+export const listDeviceStates = functions.region("europe-west1").runWith({
+  minInstances: 1,
+  memory: "128MB",
+}).https.onCall(() => {
+  listDevices();
+});
+
+/**
+ * Cloud function to list all devices in cloud iot registry
+ */
+async function listDevices() {
+  // Construct request
+  const formattedName = client.registryPath(
+      await client.getProjectId(), // project id
+      "europe-west1", // client region
+      "remotes" // registry id, my project uses only one.
+  );
+
+
+  const [response] = await client.listDevices({
+    parent: formattedName,
+  });
+  const devices = response;
+
+  if (devices.length > 0) {
+    console.log("Current devices in registry:");
+  } else {
+    console.log("No devices in registry.");
+  }
+
+  for (let i = 0; i < devices.length; i++) {
+    const device = devices[i];
+    console.log(`Device ${i}: `, device);
+  }
+}
+
+/**
+ * cloud function to send IR command to a device
+ */
 export const sendCommand = functions.region("europe-west1").runWith({
   minInstances: 1,
   memory: "128MB",
@@ -50,6 +89,9 @@ async function sendToDevice(deviceId:string, command:string) {
   return response;
 }
 
+/**
+ * cloud function to run when a device publishes to pubsub topic "remote-data"
+ */
 exports.devicePublish = functions.region("europe-west1").runWith({
   minInstances: 1,
   memory: "128MB",
